59c2c02891d4acd6b27ced09ad0453fc9c820856
Aaron <aaron@elw.com>
2019-04-24 11:33:14 +0800

    unrestrict register1, changed API URL

--- app/app.config.js
+++ app/app.config.js
@@ -73,7 +73,7 @@
       .run(['$rootScope', '$location', 'AuthService', function($rootScope, $location, AuthService) {
             $rootScope.$on('$locationChangeStart', function (event) {
                 // redirect to login page if not logged in and trying to access a restricted page
-                var restrictedPage = $.inArray($location.path(), ['/login', '/forgot', '/register', '/verify']) === -1;
+                var restrictedPage = $.inArray($location.path(), ['/login', '/forgot', '/register', '/verify', '/register1']) === -1;
                 var loggedIn = AuthService.isAuthenticated();
                 if (restrictedPage) {
                     if(!loggedIn) {
